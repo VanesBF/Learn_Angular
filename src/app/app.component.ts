7ebc15f278ef2726064aff97d97eba2dfd56c5a6
VanesBF <fomena.vanes@gmail.com>
2022-08-30 19:24:07 +0200

    new update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FaceSnap } from './models/face-snap.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  mySnap!: FaceSnap;
+
+  ngOnInit() {
+    this.mySnap = new FaceSnap(
+      'Archibald',
+      'Mon amie depuis toute petite !',
+      'https://media.routard.com/image/90/8/photo.1457908.jpg',
+      new Date(),
+      0
+    );
+  }
+
+}
+
+
+
--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FaceSnap } from '../models/face-snap.model';
 
 @Component({
   selector: 'app-face-snap',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./face-snap.component.scss']
 })
 export class FaceSnapComponent  {
+  @Input() faceSnap!: FaceSnap;
+
   tittle!: string;
   description!: string;
   createdDate!: Date;
