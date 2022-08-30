c91b047fb880b7867a13c4de1109795ead3a4c18
VanesBF <fomena.vanes@gmail.com>
2022-08-30 10:55:48 +0200

    component create

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-face-snap',
   templateUrl: './face-snap.component.html',
   styleUrls: ['./face-snap.component.scss']
 })
-export class FaceSnapComponent  {}
+export class FaceSnapComponent  {
+  tittle!: string;
+  description!: string;
+  createdDate!: Date;
+  snaps!: number;
+  imageUrl!: string;
+
+  ngOnInit(){
+   this.tittle = 'Vanessa';
+   this.description = 'Mon amie depuis toute petite!'; 
+   this.createdDate = new Date();
+   this.snaps = 6;
+   this.imageUrl = 'https://media.routard.com/image/90/8/photo.1457908.jpg'
+  }
+}
