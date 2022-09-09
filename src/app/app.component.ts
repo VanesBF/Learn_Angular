d788f7f9e27343710c37f88a470d7cff85f5034b
VanesBF <fomena.vanes@gmail.com>
2022-09-09 11:54:44 +0200

    Affichage des listes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,38 +8,38 @@ import { FaceSnap } from './models/face-snap.model';
 })
 export class AppComponent implements OnInit {
 
+  faceSnaps!: FaceSnap[]; 
   
-  mySnap!: FaceSnap;
-  myOtherSnap!: FaceSnap;
-  myLastSnap!: FaceSnap;
-
   ngOnInit() {
-    this.mySnap = { 
-      title: 'couche du soleil',
-      description: 'Meilleur couche de soleil !',
-      imageUrl: 'https://media.routard.com/image/90/8/photo.1457908.jpg',
-      createdDate: new Date(),
-      snaps: 0 ,
-      location: 'La plage'
+    this.faceSnaps = [
+      { 
+        title: 'couche du soleil',
+        description: 'Meilleur couche de soleil !',
+        imageUrl: 'https://media.routard.com/image/90/8/photo.1457908.jpg',
+        createdDate: new Date(),
+        snaps: 0 ,
+        location: 'La plage'
+  
+      },
+      { 
+        title: 'Un bon repas',
+        description: 'Mon plus bon repas !',
+        imageUrl: 'https://simple-lifes.com/wp-content/uploads/2021/01/taro-sauce-jaune-300x173.jpg',
+        createdDate: new Date(),
+        snaps: 0 ,
+        location: 'La chefferie'
+      },
 
-    };
+      { 
+        title: 'Ma ville ',
+        description: 'Une des plus belles villes du monde !',
+        imageUrl: 'https://i.pinimg.com/originals/7d/96/b4/7d96b446042f2fcf02b953ccacc2d2f1.jpg',
+        createdDate: new Date(),
+        snaps: 0
+      }
 
-    this.myOtherSnap = { 
-      title: 'Un bon repas',
-      description: 'Mon plus bon repas !',
-      imageUrl: 'https://simple-lifes.com/wp-content/uploads/2021/01/taro-sauce-jaune-300x173.jpg',
-      createdDate: new Date(),
-      snaps: 0 ,
-      location: 'La chefferie'
-    };
-
-    this.myLastSnap = { 
-      title: 'Ma ville ',
-      description: 'Une des plus belles villes du monde !',
-      imageUrl: 'https://i.pinimg.com/originals/7d/96/b4/7d96b446042f2fcf02b953ccacc2d2f1.jpg',
-      createdDate: new Date(),
-      snaps: 0
-    };
+    ];
+   
   }
 
 }
